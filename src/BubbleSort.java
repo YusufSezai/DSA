859c4c644e0da4584153a3c4cdac18096249908e
YusufSezai <yusufsezaidogan@gmail.com>
2024-06-22 14:52:34 +0300

    sort me

--- src/BubbleSort.java
+++ src/BubbleSort.java
@@ -1,2 +1,28 @@
+import java.util.Arrays;
+
 public class BubbleSort {
+    public static void main(String[] args) {
+
+        int[] numbers = {3,5,1,7,4,2};
+
+        bubbleSort(numbers);
+
+        System.out.println(Arrays.toString(numbers));
+
+    }
+
+    public static void bubbleSort(int[] array) {
+
+        for (int i = array.length - 1; i > 0; i--) {
+            for (int j = 0; j < i; j++) {
+                if (array[j] > array[j +1]) {
+                    int temp = array[j];
+                    array[j] = array[j + 1];
+                    array[j+1] = temp;
+                }
+            }
+        }
+    }
+
+
 }
